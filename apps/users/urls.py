c94c96aca0ee7a7a045bb7c1259281188ab1dc1f
peiqing.guo <peiqing.guo@iston.ai>
2023-02-03 16:35:01 +0800

    feat:修改密码

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path, include
 from rest_framework import routers
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
 
-from apps.users.views import TestAPIView, UserViewSets
+from apps.users.views import UserViewSets
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'users', UserViewSets)
@@ -12,5 +16,4 @@ urlpatterns = [
     path('login', TokenObtainPairView.as_view(), name='token_obtain_pair'),  # 系统自带
     path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify', TokenVerifyView.as_view(), name='token_verify'),
-    path("test", TestAPIView.as_view()),
 ]
--- apps/users/views.py
+++ apps/users/views.py
@@ -1,15 +1,10 @@
+from django.contrib.auth.hashers import check_password
+from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.views import APIView
 from rest_framework.viewsets import ModelViewSet
-from rest_framework_simplejwt.views import TokenObtainPairView
 
 from apps.users.models import Users
-from apps.users.serializers import UserModelSerializers, MyTokenObtainPairSerializer, UserTestModelSerializers
-
-
-class TestAPIView(APIView):
-    def get(self, request):
-        return Response({"code": 200, "msg": "测试通过."})
+from apps.users.serializers import UserModelSerializers
 
 
 class UserViewSets(ModelViewSet):
@@ -18,13 +13,25 @@ class UserViewSets(ModelViewSet):
     authentication_classes = []
     permission_classes = []
 
+    def update(self, request, *args, **kwargs):
 
-class UserLoginAPIView(TokenObtainPairView):
-    serializer_class = UserTestModelSerializers
-    authentication_classes = ()
-    permission_classes = ()
+        # 主键检验
+        pk = self.kwargs.get("pk", 0)
+        if not str(pk).isdigit():
+            return Response({"code": "400", "message": "类型错误"}, status=status.HTTP_400_BAD_REQUEST)
 
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        return Response({"code": "200", "data": serializer.data})
+        _user = self.queryset.filter(id=pk)
+        if not _user:
+            return Response({"code": "400", "message": "一个不存在的账号id."})
+
+        # 修改用户密码
+        data = self.request.data
+        pwd = data.get("password", "")
+        user = Users.objects.get(id=pk)
+
+        if check_password(pwd, user.password):
+            return Response({"code": "400", "message": "新密码不能和旧密码一样."})
+
+        user.set_password(pwd)
+        user.save()
+        return Response({"code": 200, "message": "密码修改成功."})
