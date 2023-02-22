fb10ac8e416338a22b1ac037d037a8e3c2bd3671
peiqing.guo <peiqing.guo@iston.ai>
2023-02-22 19:30:04 +0800

    test:分词分句

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -6,7 +6,7 @@ from rest_framework_simplejwt.views import (
     TokenVerifyView,
 )
 
-from apps.users.views import UserViewSets, TestSimpleJwt
+from apps.users.views import UserViewSets, TestSimpleJwt, EssayPretreatment
 
 router = routers.SimpleRouter(trailing_slash=False)
 router.register(r'users', UserViewSets)
@@ -17,4 +17,5 @@ urlpatterns = [
     path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
     path('verify', TokenVerifyView.as_view(), name='token_verify'),
     path('checkJwt', TestSimpleJwt.as_view(), name='校验 simple jwt'),
+    path('eaasyPretreatment', EssayPretreatment.as_view()),
 ]
